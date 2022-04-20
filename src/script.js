55eff1b0874387e7ec8fa38f204d63c7b6dbfaf7
Vadim <vadoq8512@gmail.com>
2022-04-21 11:00:38 +1200

    ET-36: combined faceAPI and filtered canvases and added loop that prints pixels of the eye(NEEDS TO BE CHANGED)

--- src/script.js
+++ src/script.js
@@ -50,12 +50,12 @@ video.addEventListener('play', () => {
     document.body.append(filteredCanvas)
 
     // create third canvas for adding filters
-    const faceApiCanvas = faceapi.createCanvasFromMedia(video)
-    faceApiCanvas.id = "faceApiCanvas"
-    faceApiCanvas.style.position = "absolute"
-    faceApiCanvas.style.top = 560 + "px"
-    faceApiCanvas.style.left = 0 + "px"
-    document.body.append(faceApiCanvas)
+    // const faceApiCanvas = faceapi.createCanvasFromMedia(video)
+    // faceApiCanvas.id = "faceApiCanvas"
+    // faceApiCanvas.style.position = "absolute"
+    // faceApiCanvas.style.top = 560 + "px"
+    // faceApiCanvas.style.left = 0 + "px"
+    // document.body.append(faceApiCanvas)
 
     // Set faceapi dimensions
     const displaySize = { width: video.width, height: video.height }
@@ -68,9 +68,9 @@ video.addEventListener('play', () => {
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
         
         // drawing detection and landmarks on clear canvas
-        faceApiCanvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(faceApiCanvas, resizedDetections)
-        faceapi.draw.drawFaceLandmarks(faceApiCanvas, resizedDetections)
+        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+        // faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
         
         // OpenCV adaptive threshold filter applied to the video 
         const imgSrc = cv.imread('videoCanvas');
@@ -80,11 +80,9 @@ video.addEventListener('play', () => {
         cv.adaptiveThreshold(imgSrc, dst, 200, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 35, 2);
         cv.imshow('canvasOutput', dst);
         
-        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+        // canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
         imgSrc.delete();
         dst.delete();
-        
-
 
         // extracting eye data
         if (detections.length === 1) {
@@ -94,8 +92,25 @@ video.addEventListener('play', () => {
 
             const leftEyeResized = resizedDetections[0].landmarks.getLeftEye()
             const rightEyeResized = resizedDetections[0].landmarks.getRightEye()
+
+            console.log(leftEyeResized, rightEyeResized)
+            
+            // leftEyeResized[0],change [X] the value later as it is not representetive, use max, min function instead to find the min and max coordinate in the array
+            // also want to try drawing this matrix on canvas to see what we get
+            for (let x = parseInt(leftEyeResized[0].x); x < parseInt(leftEyeResized[3].x); x ++){
+                for (let y = parseInt(leftEyeResized[1].y); y < parseInt(leftEyeResized[5].y); y ++){
+                    let src = cv.imread("canvasOutput");
+                    let pixel = src.ucharPtr(x, y);
+                    let R = pixel[0];
+                    let G = pixel[1];
+                    let B = pixel[2];
+                    let A = pixel[3];
+                    console.log(R,G,B,A,x,y)
+                }
+            }
+
         }
 
-        // console.log(leftEye, rightEye, leftEyeResized, rightEyeResized)
+
     }, 30)
 })
