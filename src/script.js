15a4f86bfc1f1050d03842207a305c7db2a908d9
terentiy8512 <vadoq8512@gmail.com>
2022-05-02 21:42:25 +1200

    added new canvases that extract eyes separately, added step 1 and step 2 of CDF implementation. Implemented minimum filter.

--- src/script.js
+++ src/script.js
@@ -42,14 +42,33 @@ video.addEventListener('play', () => {
     document.body.append(canvas)
 
     // create second canvas for adding filters
-    const croppedCanvas = document.createElement("CANVAS")
-    croppedCanvas.id = "canvasOutput"
-    croppedCanvas.width = 500
-    croppedCanvas.height = 150
-    croppedCanvas.style.position = "absolute"
-    croppedCanvas.style.top = 0 + "px"
-    croppedCanvas.style.left = 750 + "px"
-    document.body.append(croppedCanvas)
+    const croppedCanvasLeft = document.createElement("CANVAS")
+    croppedCanvasLeft.id = "canvasOutputLeft"
+    croppedCanvasLeft.width = 200
+    croppedCanvasLeft.height = 200
+    croppedCanvasLeft.style.position = "absolute"
+    croppedCanvasLeft.style.top = 0 + "px"
+    croppedCanvasLeft.style.left = 750 + "px"
+    document.body.append(croppedCanvasLeft)
+
+    const croppedCanvasRight = document.createElement("CANVAS")
+    croppedCanvasRight.id = "canvasOutputRight"
+    croppedCanvasRight.width = 200
+    croppedCanvasRight.height = 200
+    croppedCanvasRight.style.position = "absolute"
+    croppedCanvasRight.style.top = 0 + "px"
+    croppedCanvasRight.style.left = 950 + "px"
+    document.body.append(croppedCanvasRight)
+
+    // create second canvas for adding filters
+    const croppedCanvas2 = document.createElement("CANVAS")
+    croppedCanvas2.id = "canvasOutput2Left"
+    croppedCanvas2.width = 200
+    croppedCanvas2.height = 200
+    croppedCanvas2.style.position = "absolute"
+    croppedCanvas2.style.top = 200 + "px"
+    croppedCanvas2.style.left = 750 + "px"
+    document.body.append(croppedCanvas2)
 
     // Set faceapi dimensions
     const displaySize = { width: video.width, height: video.height }
@@ -72,92 +91,192 @@ video.addEventListener('play', () => {
             const leftEye = detections[0].landmarks.getLeftEye()
             const rightEye = detections[0].landmarks.getRightEye()
 
-            const [startX, startY, disX, disY] = calculateStartAndDistance(leftEye, rightEye, 5)
+            const padding = 5
+
+            const [leftStartX, leftStartY, leftDisX, leftDisY] = calculateStartAndDistance(leftEye, padding)
+
+            const [rightStartX, rightStartY, rightDisX, rightDisY] = calculateStartAndDistance(rightEye, padding)
 
             // draw cropped video onto canvas
-            croppedCanvas.getContext('2d').drawImage(
+            croppedCanvasLeft.getContext('2d').drawImage(
                 video,
-                startX, startY,                                 // start position
-                disX, disY,                                     // area to crop
+                leftStartX, leftStartY,                                 // start position
+                leftDisX, leftDisY,                                     // area to crop
                 0, 0,                                           // draw from point (0, 0) in the canvas,
-                croppedCanvas.width, croppedCanvas.height
+                croppedCanvasLeft.width, croppedCanvasLeft.height
+            )
+
+            croppedCanvasRight.getContext('2d').drawImage(
+                video,
+                rightStartX, rightStartY,                                 // start position
+                rightDisX, rightDisY,                                     // area to crop
+                0, 0,                                           // draw from point (0, 0) in the canvas,
+                croppedCanvasRight.width, croppedCanvasRight.height
             )
 
 
             // OpenCV adaptive threshold filter applied to the video 
-            const imgSrc = cv.imread('canvasOutput')
+            const imgSrc = cv.imread('canvasOutputLeft')
             let dst = new cv.Mat()
-            cv.cvtColor(imgSrc, imgSrc, cv.COLOR_RGBA2GRAY, 0)
-            cv.adaptiveThreshold(imgSrc, dst, 200, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 35, 2)
-            cv.imshow('canvasOutput', dst)
+            cv.cvtColor(imgSrc, dst, cv.COLOR_RGBA2GRAY, 0)
+            cv.imshow('canvasOutputLeft', dst)
+
+            // !remove later!
+            cv.imshow('canvasOutput2Left', dst)
+            // ---------------
             imgSrc.delete()
             dst.delete()
 
 
-            let canvas = document.getElementById("canvasOutput")
+            let canvas = document.getElementById("canvasOutputLeft")
             let ctx = canvas.getContext('2d')
-            let imgData = ctx.getImageData(0, 0, croppedCanvas.width, croppedCanvas.height)
+            let imgData = ctx.getImageData(0, 0, croppedCanvasLeft.width, croppedCanvasLeft.height)
+
+            // // brightest possible value Pixel with Minimum Intensity
+            // let pmi = 255
+            // // if nothing detected
+            // let pmiIndex = -1
+            // pixelNum = 0
+            for (let i = 3; i<imgData.data.length; i+=4) {
+
+                // Parameter we can change 0.03
+                if ((1.0 / parseFloat(imgData.data[i-1])) < 0.04){
+                    imgData.data[i-3] = 0
+                    imgData.data[i-2] = 0
+                    imgData.data[i-1] = 0
+                }else{
+                    // if (imgData.data[i-1] < pmi){
+                    //     pmi = imgData.data[i-1]
+                    //     pmiIndex = pixelNum
+                    // }
+                    imgData.data[i-3] = 255
+                    imgData.data[i-2] = 255
+                    imgData.data[i-1] = 255
+                }
+                // pixelNum ++
+            }
+            
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            canvas.width = imgData.width;
+            canvas.height = imgData.height;
+            ctx.putImageData(imgData, 0, 0);
+
+            // minimim filter (SEARCH FOR MORE FILTERS TO APPLY)
+            src = cv.imread('canvasOutputLeft');
+            dst = new cv.Mat();
+            let M = cv.Mat.ones(5, 5, cv.CV_8U);
+            let anchor = new cv.Point(-1, -1);
+
+            // You can try more different parameters
+            cv.erode(src, dst, M, anchor, 2, cv.BORDER_ISOLATED, cv.morphologyDefaultBorderValue());
+            cv.imshow('canvasOutputLeft', dst);
+            src.delete(); dst.delete(); M.delete();
+
+
+            // brightest possible value Pixel with Minimum Intensity
+            let pmi = 255
+            // if nothing detected
+            let pmiIndex = -1
+            let pixelNum = 0
+
+            for (let i = 3; i<imgData.data.length; i+=4) {
+                if (imgData.data[i-1] == 255){
+                    if (imgData.data[i-1] < pmi){
+                        pmi = imgData.data[i-1]
+                        pmiIndex = pixelNum
+                    }
+                }
+                pixelNum++
+            }
+            console.log(pmi, pmiIndex)
+
+            row = Math.floor(pmiIndex/(croppedCanvasLeft.width))
+            column = pmiIndex % (croppedCanvasLeft.width)
+
+
+            // mainImageColumn = leftStartX + padding + column/leftDisX
+            // mainImageRow = leftStartY - padding + row/leftDisY
+            // console.log(row, column, mainImageRow, mainImageColumn)
+
+
+            // let c = document.getElementById("canvasOutputLeft")
+            // let ctxx = c.getContext("2d")
+            // ctxx.beginPath()
+            // ctxx.arc(row, column, 5, 0, 2 * Math.PI)
+            // ctxx.stroke()
 
             // array [R,G,B,A,R,G,B,A,...]
-            let matrix = cv.matFromImageData(imgData).data
-
-            //WARNING: tried to make it in a more readable format, however going through the matrix just one time 
-            // takes so much time(making it laggy) since the resolution is high, therefore the array consists of 80000 pixels making it total of 80000*4=320000 elements in the array.
-            // so if we want to work futher with the matrix we need to 1. reduce resolution, 2. reduce canvas width and height, 3. Consider cropping eyes separately (reducing canvas size)
-
-            // counter = 0
-            // rgba_array = []
-            // // array [[R,G,B,A], [R,G,B,A]]
-            // readable_matrix = []
-            // for (component_key in matrix){
-            //     if (counter == 4){
-            //         readable_matrix.push(rgba_array)
-            //         rgba_array = []
-            //         counter = 0
-            //     }
-            //     rgba_array.push(matrix[component_key])
-            //     counter++
-            // }
-
-            // console.log(readable_matrix)
+            // let matrix = cv.matFromImageData(imgData).data
 
         }
 
     }, 30)
 })
 
+
+
+// /**
+//  * Finds the starting and ending x,y coordinates from the left eye to the right eye.
+//  * 
+//  * @param {Object}  leftEye leftEye variable is a dictionary of x and y coordinates
+//  * @param {Object}  rightEye rightEye variable is a dictionary of x and y coordinates
+//  * @param {number}  padding padding added to the left and right
+//  * 
+//  * @return {number} Returns an array of [startX, startY, disX, disY]
+//  */
+// const calculateStartAndDistance = (leftEye, rightEye, padding) => {
+
+//     // Calculations for leftEye
+//     // Place x and y coords into seperate arrays
+//     const leftEyeXcoord = leftEye.map(i => i.x);
+//     const leftEyeYcoord = leftEye.map(i => i.y);
+
+//     let minX = Math.min(...leftEyeXcoord) - padding
+//     let minY1 = Math.min(...leftEyeYcoord)
+//     let maxY1 = Math.max(...leftEyeYcoord)
+
+//     // Calculations for rightEye
+//     const rightEyeXcoord = rightEye.map(i => i.x);
+//     const rightEyeYcoord = rightEye.map(i => i.y);
+
+//     let maxX = Math.max(...rightEyeXcoord) + padding
+//     let minY2 = Math.min(...rightEyeYcoord)
+//     let maxY2 = Math.max(...rightEyeYcoord)
+
+//     // If you rotate your head, Y position of the left and the right eye will change, 
+//     // sometimes left eye will have min coordinate and sometimes right eye will have min,
+//     // same for max coordinate
+//     // Determine whether minY, maxY is on the right eye or on the left eye 
+//     let minY = Math.min(minY1, minY2) - padding
+//     let maxY = Math.max(maxY1, maxY2) + padding
+
+//     return [minX, minY, maxX - minX, maxY - minY]
+// }
+
+
 /**
  * Finds the starting and ending x,y coordinates from the left eye to the right eye.
  * 
- * @param {Object}  leftEye leftEye variable is a dictionary of x and y coordinates
- * @param {Object}  rightEye rightEye variable is a dictionary of x and y coordinates
- * @param {number}  padding padding added to the left and right
+ * @param {Object}  eye eye variable is a dictionary of x and y coordinates
+ * @param {number}  padding padding added to image
  * 
  * @return {number} Returns an array of [startX, startY, disX, disY]
  */
-const calculateStartAndDistance = (leftEye, rightEye, padding) => {
+const calculateStartAndDistance = (eye, padding) => {
 
-    // Calculations for leftEye
     // Place x and y coords into seperate arrays
-    const leftEyeXcoord = leftEye.map(i => i.x);
-    const leftEyeYcoord = leftEye.map(i => i.y);
+    const EyeXcoord = eye.map(i => i.x);
+    const EyeYcoord = eye.map(i => i.y);
 
-    let minX = Math.min(...leftEyeXcoord) - padding
-    let minY1 = Math.min(...leftEyeYcoord)
-    let maxY1 = Math.max(...leftEyeYcoord)
+    let minX = Math.min(...EyeXcoord) - padding
+    let minY1 = Math.min(...EyeYcoord)
+    let maxY1 = Math.max(...EyeYcoord)
 
-    // Calculations for rightEye
-    const rightEyeXcoord = rightEye.map(i => i.x);
-    const rightEyeYcoord = rightEye.map(i => i.y);
+    let maxX = Math.max(...EyeXcoord) + padding
+    let minY2 = Math.min(...EyeYcoord)
+    let maxY2 = Math.max(...EyeYcoord)
 
-    let maxX = Math.max(...rightEyeXcoord) + padding
-    let minY2 = Math.min(...rightEyeYcoord)
-    let maxY2 = Math.max(...rightEyeYcoord)
-
-    // If you rotate your head, Y position of the left and the right eye will change, 
-    // sometimes left eye will have min coordinate and sometimes right eye will have min,
-    // same for max coordinate
-    // Determine whether minY, maxY is on the right eye or on the left eye 
     let minY = Math.min(minY1, minY2) - padding
     let maxY = Math.max(maxY1, maxY2) + padding
 
