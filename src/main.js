7ab4978df9b03b9e2bd49928438bea67d18042a5
Amri <amri.arshad16@yahoo.com>
2022-05-11 21:12:30 +1200

    ET-60: extracted eye detection + image processing to seperate files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+import { drawCroppedCanvases } from './modules/eyeDetection.js'
 import { clearCanvas, croppedCanvas2, croppedCanvasLeft, croppedCanvasRight, video } from './modules/elements.js'
+import { applyImageProcessing } from './modules/imageProcessing.js'
 
 // Faceapi eye points
 // const LEFT_EYE_POINTS = [36, 37, 38, 39, 40, 41]
@@ -54,46 +56,12 @@ video.addEventListener('play', () => {
             document.body.append(croppedCanvasRight)
             document.body.append(croppedCanvas2)
 
-            // get left and right eye
-            const leftEye = detections[0].landmarks.getLeftEye()
-            const rightEye = detections[0].landmarks.getRightEye()
+            // Eye detection
+            drawCroppedCanvases(detections)
+            // Image Processing
+            applyImageProcessing()
 
-            const padding = 5
-
-            const [leftStartX, leftStartY, leftDisX, leftDisY] = calculateStartAndDistance(leftEye, padding)
-
-            const [rightStartX, rightStartY, rightDisX, rightDisY] = calculateStartAndDistance(rightEye, padding)
-
-            // draw cropped video onto canvas
-            croppedCanvasLeft.getContext('2d').drawImage(
-                video,
-                leftStartX, leftStartY,                                 // start position
-                leftDisX, leftDisY,                                     // area to crop
-                0, 0,                                                   // draw from point (0, 0) in the canvas,
-                croppedCanvasLeft.width, croppedCanvasLeft.height
-            )
-
-            croppedCanvasRight.getContext('2d').drawImage(
-                video,
-                rightStartX, rightStartY,                                 // start position
-                rightDisX, rightDisY,                                     // area to crop
-                0, 0,                                                     // draw from point (0, 0) in the canvas,
-                croppedCanvasRight.width, croppedCanvasRight.height
-            )
-
-            // OpenCV adaptive threshold filter applied to the video 
-            const imgSrc = cv.imread('canvasOutputLeft')
-            let dst = new cv.Mat()
-            cv.cvtColor(imgSrc, dst, cv.COLOR_RGBA2GRAY, 0)
-            cv.imshow('canvasOutputLeft', dst)
-
-            // debugging canvas
-            cv.imshow('canvasOutput2Left', dst)
-            // ---------------
-
-            imgSrc.delete()
-            dst.delete()
-
+            // ------------------------ PUPIL DETECTION -----------------------------
             let ctx = croppedCanvasLeft.getContext('2d')
             let imgData = ctx.getImageData(0, 0, croppedCanvasLeft.width, croppedCanvasLeft.height)
             // Make a copy of original grayscale data
@@ -103,10 +71,8 @@ video.addEventListener('play', () => {
 
             // Getting the input value from the slider
             const sliderInputValue = document.getElementById("myRange")
-
             // Grabs value from value from sliderInputValue and approximates conversion            
             const intensityThreshold = sliderInputValue.value / 1000
-
             const output = document.getElementById("intensityThreshold")
             output.innerHTML = intensityThreshold
 
@@ -131,7 +97,7 @@ video.addEventListener('play', () => {
 
             // minimim filter applied (SEARCH FOR MORE FILTERS TO APPLY)
             const src = cv.imread('canvasOutputLeft')
-            dst = new cv.Mat()
+            let dst = new cv.Mat()
             let M = cv.Mat.ones(5, 5, cv.CV_8U)
             let anchor = new cv.Point(-1, -1)
             // You can try more different parameters
@@ -205,7 +171,6 @@ video.addEventListener('play', () => {
                 const newPmiX = totalX / counter
                 const newPmiY = totalY / counter
 
-
                 // coordinates for main image
                 // mainImageColumn = leftStartX + padding + column / leftDisX
                 // mainImageRow = leftStartY - padding + row / leftDisY
@@ -246,8 +211,6 @@ video.addEventListener('play', () => {
     }, 30)
 })
 
-
-
 // /**
 //  * Finds the starting and ending x,y coordinates from the left eye to the right eye.
 //  * 
@@ -286,7 +249,6 @@ video.addEventListener('play', () => {
 //     return [minX, minY, maxX - minX, maxY - minY]
 // }
 
-
 /** Calculates the average intensity in a (size x size) pixel square around the PMI
 * 
 * @param {array}  grayScaleMatrix 2D array of pixels
@@ -308,33 +270,4 @@ const getAverageIntensity = (grayScaleMatrix, x, y, size, canvas) => {
         }
     }
     return totalIntensity / counter
-}
-
-
-/**
- * Finds the starting and ending x,y coordinates from the left eye to the right eye.
- * 
- * @param {Object}  eye eye variable is a dictionary of x and y coordinates
- * @param {number}  padding padding added to image
- * 
- * @return {number} Returns an array of [startX, startY, disX, disY]
- */
-const calculateStartAndDistance = (eye, padding) => {
-
-    // Place x and y coords into seperate arrays
-    const EyeXcoord = eye.map(i => i.x)
-    const EyeYcoord = eye.map(i => i.y)
-
-    let minX = Math.min(...EyeXcoord) - padding
-    let minY1 = Math.min(...EyeYcoord)
-    let maxY1 = Math.max(...EyeYcoord)
-
-    let maxX = Math.max(...EyeXcoord) + padding
-    let minY2 = Math.min(...EyeYcoord)
-    let maxY2 = Math.max(...EyeYcoord)
-
-    let minY = Math.min(minY1, minY2) - padding
-    let maxY = Math.max(maxY1, maxY2) + padding
-
-    return [minX, minY, maxX - minX, maxY - minY]
 }
---
+++ src/modules/eyeDetection.js
@@ -0,0 +1,64 @@
+
+import { croppedCanvasLeft, croppedCanvasRight } from "./elements.js"
+
+/**
+ * Draws a cropped canvas for each eye
+ * 
+ * @param {any} detections 
+ */
+const drawCroppedCanvases = (detections) => {
+  // get left and right eye
+  const leftEye = detections[0].landmarks.getLeftEye()
+  const rightEye = detections[0].landmarks.getRightEye()
+  const padding = 5
+
+  const [leftStartX, leftStartY, leftDisX, leftDisY] = calculateStartAndDistance(leftEye, padding)
+  const [rightStartX, rightStartY, rightDisX, rightDisY] = calculateStartAndDistance(rightEye, padding)
+
+  // draw cropped video onto canvas
+  croppedCanvasLeft.getContext('2d').drawImage(
+    video,
+    leftStartX, leftStartY,                                 // start position
+    leftDisX, leftDisY,                                     // area to crop
+    0, 0,                                                   // draw from point (0, 0) in the canvas,
+    croppedCanvasLeft.width, croppedCanvasLeft.height
+  )
+
+  croppedCanvasRight.getContext('2d').drawImage(
+    video,
+    rightStartX, rightStartY,                                 // start position
+    rightDisX, rightDisY,                                     // area to crop
+    0, 0,                                                     // draw from point (0, 0) in the canvas,
+    croppedCanvasRight.width, croppedCanvasRight.height
+  )
+}
+
+/**
+ * Finds the starting and ending x,y coordinates from the left eye to the right eye.
+ * 
+ * @param {Object}  eye eye variable is a dictionary of x and y coordinates
+ * @param {number}  padding padding added to image
+ * 
+ * @return {number} Returns an array of [startX, startY, disX, disY]
+ */
+const calculateStartAndDistance = (eye, padding) => {
+
+  // Place x and y coords into seperate arrays
+  const EyeXcoord = eye.map(i => i.x)
+  const EyeYcoord = eye.map(i => i.y)
+
+  let minX = Math.min(...EyeXcoord) - padding
+  let minY1 = Math.min(...EyeYcoord)
+  let maxY1 = Math.max(...EyeYcoord)
+
+  let maxX = Math.max(...EyeXcoord) + padding
+  let minY2 = Math.min(...EyeYcoord)
+  let maxY2 = Math.max(...EyeYcoord)
+
+  let minY = Math.min(minY1, minY2) - padding
+  let maxY = Math.max(maxY1, maxY2) + padding
+
+  return [minX, minY, maxX - minX, maxY - minY]
+}
+
+export { drawCroppedCanvases }
