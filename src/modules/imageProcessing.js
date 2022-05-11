c3ce0ebdad86686490a975fa19d10221ef25201e
Amri <amri.arshad16@yahoo.com>
2022-05-11 21:12:30 +1200

    extracted eye detection + image processing to seperate files

---
+++ src/modules/imageProcessing.js
@@ -0,0 +1,15 @@
+const applyImageProcessing = () => {
+  // OpenCV adaptive threshold filter applied to the video 
+  const imgSrc = cv.imread('canvasOutputLeft')
+  let dst = new cv.Mat()
+  cv.cvtColor(imgSrc, dst, cv.COLOR_RGBA2GRAY, 0)
+  cv.imshow('canvasOutputLeft', dst)
+
+  // debugging canvas
+  cv.imshow('canvasOutput2Left', dst)
+
+  imgSrc.delete()
+  dst.delete()
+}
+
+export { applyImageProcessing }
